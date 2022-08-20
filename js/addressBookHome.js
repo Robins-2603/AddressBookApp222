5547cee587cfe6d644151eb44a968976a4e345f2
Robins-2603 <robinsforwork@gmail.com>
2022-08-20 13:57:38 +0530

    Added: code for remove functionality

--- js/addressBookHome.js
+++ js/addressBookHome.js
@@ -33,3 +33,16 @@ const createInnerHtml = () => {
     }
     document.querySelector('#display').innerHTML = innerHtml;
 }
+
+// for remove option UC5
+const remove = (node) => {
+    let addrAddressData = addrBookList.find(addrData => addrData._id == node.id);
+   if(!addrAddressData) return;
+    const index = addrBookList 
+                    .map(addrData => addrData._id)
+                    .indexOf(addrAddressData._id);
+                    addrBookList.splice(index, 1);
+    localStorage.setItem('AddressBookList', JSON.stringify(addrBookList));
+    document.querySelector('.per-count').textContent = addrBookList.length;
+    createInnerHtml();
+}
