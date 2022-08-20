e36f47f157b929fad409ce34c41a81e89ed81c51
Robins-2603 <robinsforwork@gmail.com>
2022-08-20 13:53:13 +0530

    Added: code for get data from local storage

--- js/addressBookHome.js
+++ js/addressBookHome.js
@@ -1,24 +1,35 @@
+let addrBookList
 window.addEventListener('DOMContentLoaded', (event) => {
+    addrBookList = getAddressBookDataFromStorage();
+    document.querySelector(".per-count").textContent = addrBookList.length;
     createInnerHtml();
 });
 
-const createInnerHtml = () => {
-    const HeaderHtml = "<tr><th>Full Name</th><th>Address</th><th>City</th><th>State</th>" +
-    "<th>Zip Code</th><th>Phone Number</th><th>Options</th></tr>";
-    const innerHtml = `${HeaderHtml}   
-                <tr>
-                    <td>Robins Chandna</td>
-                    <td>Vijay nagar</td>
-                    <td>Ghaziabad</td>
-                    <td>Uttar Pradesh</td>
+getAddressBookDataFromStorage = () => {
+    return localStorage.getItem('AddressBookList') ? 
+                    JSON.parse(localStorage.getItem('AddressBookList')) : [];
+}
 
-                    <td>201009</td>
-                    <td>+91 0987654321</td>
-                    <td>
-                        <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                        <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
-                    </td>
-                </tr>
-                `;
+const createInnerHtml = () => {
+    if (addrBookList.length == 0) return;
+    const headerHtml = "<tr><th>Full Name</th><th>Address</th><th>City</th><th>State</th>" +
+        "<th>Zip Code</th><th>Phone Number</th><th>Options</th></tr>";
+    let innerHtml = `${headerHtml}`;
+    for (let addrBookData of addrBookList) {
+        innerHtml = `${innerHtml}
+            <tr>
+                <td>${addrBookData._name}</td>
+                <td>${addrBookData._address}</td>
+                <td>${addrBookData._city}</td>
+                <td>${addrBookData._state}</td>
+                <td>${addrBookData._zip}</td>
+                <td>${addrBookData._phone}</td>
+                <td>
+                    <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                    <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+                </td>
+            </tr>
+        `;
+    }
     document.querySelector('#display').innerHTML = innerHtml;
 }
